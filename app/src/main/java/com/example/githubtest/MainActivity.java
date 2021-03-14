21e5d9d75d643cc36bade86adee708f412269949
JW <kiw970923@naver.com>
2021-03-14 13:58:11 +0900

    a 개발자가 주석을 달았다.

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // a 개발자 부쉬
     }
 }
