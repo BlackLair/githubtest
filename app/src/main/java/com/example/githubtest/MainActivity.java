b8369c1a502a69bd5bba953621e7fca0cfe98404
JW <kiw970923@naver.com>
2021-04-08 13:30:23 +0900

    Revert "하단 레이아웃의 터치 위치 좌표를 텍스트뷰에 출력하는 기능"
    This reverts commit 95142fd8

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -3,10 +3,6 @@ package com.example.githubtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 //코밋만해봐
 
 //믹스트 지우기
@@ -16,26 +12,12 @@ import android.widget.TextView;
 //이번엔 다시 진짜 하드
 //코밋만해봐
 public class MainActivity extends AppCompatActivity {
-    LinearLayout layout;
-    TextView myText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        layout=(LinearLayout)findViewById(R.id.myLinear);
-        myText=(TextView)findViewById(R.id.locationText);
-        layout.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                float x, y;
-                if(motionEvent.getAction()==MotionEvent.ACTION_DOWN) {
-                    x = motionEvent.getX();
-                    y = motionEvent.getY();
-                    myText.setText("터치 위치 : x (" + Float.toString(x) + "), y (" + Float.toString(y) + ")");
-                }
-                return false;
-            }
-        });
+        System.out.println("!23213");
 
 // 진선우 보여줄 주석이다 이거야~
     }
