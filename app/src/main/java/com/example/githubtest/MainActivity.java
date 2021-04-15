67b0b933e91a0b86b8140d31266039fb4f59bee3
Mycom <kiw970923@naver.com>
2021-04-16 00:24:05 +0900

    버튼 누르면 색깔이 부드럽게 바뀜 !!!중요

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -2,7 +2,18 @@ package com.example.githubtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
+import android.transition.Transition;
+import android.view.View;
+import android.view.animation.AnimationSet;
+import android.widget.Button;
 //코밋만해봐
 
 //믹스트 지우기
@@ -12,12 +23,47 @@ import android.os.Bundle;
 //이번엔 다시 진짜 하드
 //코밋만해봐
 public class MainActivity extends AppCompatActivity {
+    Button myButton;
 
+    ValueAnimator colorAnimation;
+    int colorfrom, colorto, count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("!23213");
+        myButton=findViewById(R.id.myButton);
+        colorfrom=((ColorDrawable)myButton.getBackground()).getColor();
+        colorto=Color.parseColor("#0000ff");
+        colorAnimation=ValueAnimator.ofObject(new ArgbEvaluator(), colorfrom, colorto);
+
+        colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                myButton.setBackgroundColor((int)valueAnimator.getAnimatedValue());
+            }
+        });
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               // myTransition.reverseTransition(1000);
+                if(count==0) {
+                    colorAnimation.setObjectValues(((ColorDrawable) view.getBackground()).getColor(), Color.parseColor("#ff0000"));
+                    count = 1;
+                }else{
+                    colorAnimation.setObjectValues(((ColorDrawable) view.getBackground()).getColor(), Color.parseColor("#0000ff"));
+                    count=0;
+                }
+
+
+                colorAnimation.setDuration(200);
+                colorAnimation.start();
+
+
+            }
+        });
+
+
+
 
 // 진선우 보여줄 주석이다 이거야~
     }
