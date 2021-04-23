8cc8dee0f736034acc5b6af995d7ea774876d5a6
Mycom <kiw970923@naver.com>
2021-04-23 21:12:28 +0900

    하단 버튼 2개 사운드 테스트

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.githubtest;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.Animator;
@@ -9,8 +10,14 @@ import android.animation.ValueAnimator;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.TransitionDrawable;
+import android.media.AudioAttributes;
+import android.media.SoundPool;
+import android.media.audiofx.EnvironmentalReverb;
+import android.media.audiofx.PresetReverb;
+import android.os.Build;
 import android.os.Bundle;
 import android.transition.Transition;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.AnimationSet;
 import android.widget.Button;
@@ -23,15 +30,19 @@ import android.widget.Button;
 //이번엔 다시 진짜 하드
 //코밋만해봐
 public class MainActivity extends AppCompatActivity {
-    Button myButton;
-
+    Button myButton, btn_1, btn_2;
+    SoundPool spool;
     ValueAnimator colorAnimation;
     int colorfrom, colorto, count=0;
+    int soundID=0;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myButton=findViewById(R.id.myButton);
+        btn_1=findViewById(R.id.btn_1);
+        btn_2=findViewById(R.id.btn_2);
         colorfrom=((ColorDrawable)myButton.getBackground()).getColor();
         colorto=Color.parseColor("#0000ff");
         colorAnimation=ValueAnimator.ofObject(new ArgbEvaluator(), colorfrom, colorto);
@@ -53,15 +64,48 @@ public class MainActivity extends AppCompatActivity {
                     colorAnimation.setObjectValues(((ColorDrawable) view.getBackground()).getColor(), Color.parseColor("#0000ff"));
                     count=0;
                 }
-
-
                 colorAnimation.setDuration(200);
                 colorAnimation.start();
-
-
             }
         });
 
+        PresetReverb mReverb=new PresetReverb(0,0);
+        mReverb.setPreset(PresetReverb.PRESET_LARGEHALL);
+
+
+        AudioAttributes audioAttributes=new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .build();
+
+        spool=new SoundPool.Builder().setAudioAttributes(audioAttributes).setMaxStreams(5).build();
+        int key=spool.load(this, R.raw.p46, 1);
+        btn_1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction()==MotionEvent.ACTION_DOWN){
+                    mReverb.setEnabled(true);
+                    soundID=spool.play(key,1,1,0,0,1);
+                }
+                else if(motionEvent.getAction()==MotionEvent.ACTION_UP){
+                    spool.stop(soundID);
+                }
+                return false;
+            }
+        });
+        btn_2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction()==MotionEvent.ACTION_DOWN){
+                    mReverb.setEnabled(false);
+                    soundID=spool.play(key,1,1,0,0,1);
+                }
+                else if(motionEvent.getAction()==MotionEvent.ACTION_UP){
+                    spool.stop(soundID);
+                }
+                return false;
+            }
+        });
 
 
 
